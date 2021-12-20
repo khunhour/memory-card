dce5f328d0cc2381acd33375ff0fff67040372b8
Khunhour Morm <khonhourmorm@gmail.com>
2021-12-20 21:42:45 +0900

    implemented useEffect for updating best score

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,11 +1,19 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import Grid from "./grid/Grid";
 import BestScore from "./score/BestScore";
 import CurrentScore from "./score/CurrentScore";
 
 export default function Main() {
-	const [currentScore, useCurrentScore] = useState(0);
-	const [bestScore, useBestScore] = useState(0);
+	const [currentScore, setCurrentScore] = useState(0);
+	const [bestScore, setBestScore] = useState(0);
+
+	// update best score
+	useEffect(() => {
+		if (bestScore < currentScore) {
+			setBestScore(currentScore);
+		}
+	}, [currentScore, bestScore]);
+
 	return (
 		<main>
 			<div>
