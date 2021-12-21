324176d960016929e68b67f0764a4c553f1adcf2
Khunhour Morm <khonhourmorm@gmail.com>
2021-12-22 00:05:15 +0900

    edit footer style and change images location

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,5 +1,12 @@
 import React from "react";
-
+import githubPic from "../../src/images/github.png";
 export default function Footer() {
-	return <footer>Footer</footer>;
+	return (
+		<footer>
+			<span>2021 &copy; khunhour</span>
+			<a href="https://github.com/khunhour/cv-project">
+				<img src={githubPic} alt="github" />
+			</a>
+		</footer>
+	);
 }
